2b8b59b2b6aab8f5162a84da9f43ddfb16d3a3a6
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-07-27 15:04:07 -0400

    urls

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 # Create your views here.
 @api_view(['POST'])
 def send_geo_data(request):
     req_data = request.data
+    
+    
+def index(request):
+    context = {"message": "Hello"}  # Use a dictionary here
+    return render(request, "main/hi.html", context)
     
