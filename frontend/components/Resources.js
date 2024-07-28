236da20c045f49022c6f2635b35a9c46ea21bc74
joshuacris <crisologoj57@gmail.com>
2024-07-28 10:59:17 -0400

    updated navbar and suscept. query

--- frontend/components/Resources.js
+++ frontend/components/Resources.js
@@ -15,42 +15,10 @@ const Resources = (props) => {
   <div>
     
  <section className='bg-blue-frostedLight'>
- <nav class="sticky top-0 z-50 border-gray-200 bg-blue-frostedLight">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href=".." class="flex items-center space-x-3 rtl:space-x-reverse">
-      <Image
-        src={compass}
-        alt="Compass"
-        width={75}
-        height={75}
-        className="bg-transparent"
-      />
-      <span class="text-base self-center font-semibold whitespace-nowrap dark:text-black">Climate Compass (Home)</span>
-    </a>
-    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-      </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-      <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-        <li>
-          <a href="#climatemap" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Climate Map</a>
-        </li>
-        <li>
-          <a href="#migration" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Migration Services</a>
-        </li>
-        <li>
-          <a href="#moveto" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Moving to this Area?</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
 
- <div class="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
+
+ <div class="flex flex-1 w-full flex-col items-center justify-center text-center px-4">
     <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
         <span className="relative whitespace-nowrap text-blue-Figma">
             <svg aria-hidden="true" viewBox="0 0 418 42" className="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-Figma/70" preserveAspectRatio="none"><path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path></svg>
@@ -59,7 +27,7 @@ const Resources = (props) => {
     </h1>
 
     <h2 className="mt-10 mx-auto max-w-4xl font-display text-3xl font-bold tracking-normal text-slate-900 sm:text-4xl flex items-center text-left">
-      Results for {props.name} 
+      Results for (YOUR COUNTRY)
     </h2>
     <p class="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">Here, you will find the most
       <span className="font-bold"> essential </span> climate details about your
@@ -78,7 +46,8 @@ const Resources = (props) => {
 <div className="relative opacity-75">
 
 <h2 className="mt-5 text-2xl bg-white border border-black text-blue-900 font-semibold px-16 py-6 rounded-full hover:bg-blue-100 transition duration-300 text-center flex items-center justify-center">
-          {props.name}
+          {props.type}
+          {props.news}
           <span className='ml-2'>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/></svg>
           </span></h2>
@@ -117,13 +86,13 @@ const Resources = (props) => {
     <details class="mb-2 w-full">
         <summary class="text-2xl bg-gray-200 p-4 rounded-lg cursor-pointer shadow-md mb-4 w-full">
             <span class="font-semibold">Climate Shelters Nearby</span>
-
+            {props.refuge.map((location, index) => (
+              <p key={index}>{location}</p>
+            ))}
         </summary>
         <div class="shadow-2xl bg-white w-full h-96 mx-auto my-8 p-6 rounded-lg flex items-start justify-left">
         <div className="bg-white w-full">
-  <p className="text-black text-2xl font-bold">{props.refuge.map((location, index) => (
-              <p key={index}>{location}</p>
-            ))}</p>
+  <p className="text-black text-2xl font-bold">Put AI output here</p>
 </div>
         </div>
     </details>
@@ -132,17 +101,11 @@ const Resources = (props) => {
 <div class="p-4 mx-auto w-full">
     <details class="mb-2 w-full">
         <summary class="text-2xl bg-gray-200 p-4 rounded-lg cursor-pointer shadow-md mb-4 w-full">
-            <span class="font-semibold">News</span>
+            <span class="font-semibold">Recommended Areas for Relocation</span>
         </summary>
         <div class="shadow-2xl bg-white w-full h-96 mx-auto my-8 p-6 rounded-lg flex items-start justify-left">
         <div className="bg-white w-full">
- {props.news1[1]}
- {props.news1[0]}
-
-  <h1>here</h1>
-  {props.news2}
-  <h1>here</h1>
-  {props.news3}
+  <p className="text-black text-2xl font-bold">Put AI output here</p>
 </div>
         </div>
     </details>
--- mysite/main/susceptability_query.py
+++ mysite/main/susceptability_query.py
@@ -1,40 +1,45 @@
 import requests
 
 
-url = "https://api.reliefweb.int/v1/disasters"
+def susceptability_query(country):
+    url = "https://api.reliefweb.int/v1/disasters"
 
 
-params = {
-    'appname': 'YOUR_APP_NAME', 
-    'filter[field]': 'date',
-    'filter[value][from]': '2011-01-01T00:00:00+00:00',
-    'filter[value][to]': '2024-12-31T23:59:59+00:00',
-    'filter[operator]': 'and',
-    'filter[field]': 'country',
-    'filter[value]': 'Afghanistan'
-}
-
-country = params['filter[value]']
-response = requests.get(url, params=params)
-data = response.json()
+    params = {
+        'appname': 'YOUR_APP_NAME', 
+        'filter[field]': 'date',
+        'filter[value][from]': '2011-01-01T00:00:00+00:00',
+        'filter[value][to]': '2024-12-31T23:59:59+00:00',
+        'filter[operator]': 'and',
+        'filter[field]': 'country',
+        'filter[value]': country
+    }
 
 
+    response = requests.get(url, params=params)
+    data = response.json()
 
 
 
-counter = 0
-results = {}
-
-for i in range(0, len(data['data'])):
-    if ('Pandemic' not in data['data'][i]['fields']['name']):
-        results['event' + str(i)] = data['data'][i]['fields']['name']
-        counter += 1
 
 
-if (counter >= 7):
-    print("According to the ReliefWeb database for historical natural disaster data, " 
-          + country + 
-          " is susceptible to natural disasters, due to the increased amount of natural disasters in its history.\n")
-else:
-    print("According to the ReliefWeb database for historical natural disaster data, " + country + 
-          " is NOT susceptible to natural disasters, due to the lack of numerous recent disasters.\n")
+    counter = 0
+    results = {}
+
+    for i in range(0, len(data['data'])):
+        if ('Pandemic' not in data['data'][i]['fields']['name']):
+            results['event' + str(i)] = data['data'][i]['fields']['name']
+            counter += 1
+
+
+    if (counter >= 7):
+        return("According to the ReliefWeb database for historical natural disaster data, " 
+            + country + 
+            " is susceptible to natural disasters, due to the increased amount of natural disasters in its history.\n")
+    else:
+        return("According to the ReliefWeb database for historical natural disaster data, " + country + 
+            " is NOT susceptible to natural disasters, due to the lack of numerous recent disasters.\n")
+    
+
+#print output
+#print(susceptability_query("Japan"))
